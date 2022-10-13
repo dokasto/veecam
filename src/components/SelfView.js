445c2ba3e27aa4b4e5ed194a2919b1bb46c97390
Udo Nkwocha <udokah@fb.com>
2022-10-13 19:11:15 +0100

    render video stream directly to canvas

--- src/components/SelfView.js
+++ src/components/SelfView.js
@@ -1,70 +1,25 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { createUseStyles } from "react-jss";
-import MediaStreamContext from "../data_providers/MediaStreamContext";
-import { useRenderVideoToCanvas } from "../hooks/canvasHooks";
+import { useRenderStreamToCanvas } from "../hooks/canvasHooks";
 
 const useStyles = createUseStyles({
   root: {
     marginTop: 20,
-    backgroundColor: "#ecf0f1",
     borderRadius: 4,
     width: 680,
+    textAlign: "center",
+    overflow: "hidden",
   },
 });
 
 export default function SelfView() {
   const classes = useStyles();
-  const { stream } = useContext(MediaStreamContext);
-  const videoRef = useRef(null);
   const canvasRef = useRef(null);
-  const isVideoReady = useRef(false);
-  const requestAnimationFrameRef = useRef(null);
-  const renderVideoToCanvas = useRenderVideoToCanvas();
-  const hasStartedRenderLoop = useRef(false);
-  const imageCaptureRef = useRef(null);
-
-  useEffect(() => {
-    if (stream != null && videoRef != null) {
-      videoRef.current.srcObject = stream;
-      videoRef.current.play();
-      isVideoReady.current = true;
-      const [track] = stream.getVideoTracks();
-      imageCaptureRef.current = new ImageCapture(track);
-      imageCaptureRef.current.grabFrame().then((imageBitmap) => {
-        renderVideoToCanvas(canvasRef.current, imageBitmap);
-      });
-    }
-  }, [renderVideoToCanvas, stream]);
-
-  // useEffect(() => {
-  //   if (!isVideoReady.current && hasStartedRenderLoop.current) {
-  //     return;
-  //   }
-
-  //   hasStartedRenderLoop.current = true;
-
-  //   function render() {
-  //     if (
-  //       canvasRef.current != null &&
-  //       videoRef.current != null &&
-  //       isVideoReady.current
-  //     ) {
-  //       console.log("track", track);
-  //     }
-  //     requestAnimationFrameRef.current = window.requestAnimationFrame(render);
-  //   }
-
-  //   requestAnimationFrameRef.current = window.requestAnimationFrame(render);
-
-  //   return () => window.cancelAnimationFrame(requestAnimationFrameRef.current);
-  // }, [renderVideoToCanvas, stream]);
-
-  // useRenderVideoToCanvas(videoRef, canvasRef);
+  useRenderStreamToCanvas(canvasRef.current);
 
   return (
     <div className={classes.root}>
       <canvas ref={canvasRef} id="canvas"></canvas>
-      <video ref={videoRef} src={stream} id="video"></video>
     </div>
   );
 }
--- src/hooks/canvasHooks.js
+++ src/hooks/canvasHooks.js
@@ -1,30 +1,47 @@
-import { useRef, useCallback } from "react";
-import OffscreenCanvasWorker from "web-worker:../workers/OffscreenCanvasWorker";
+import { useRef, useContext, useEffect } from "react";
+// import OffscreenCanvasWorker from "web-worker:../workers/OffscreenCanvasWorker";
+import MediaStreamContext from "../data_providers/MediaStreamContext";
 
-export function useRenderVideoToCanvas() {
-  const offscreenRef = useRef(null);
-  const workerRef = useRef(new OffscreenCanvasWorker());
-  const hasTransferedControlRef = useRef(false);
+export function useRenderStreamToCanvas(canvas) {
+  const { stream } = useContext(MediaStreamContext);
+  const imageCaptureRef = useRef(null);
+  const videoTrackRef = useRef(null);
+  const canvasContextRef = useRef(null);
+  const requestAnimationFrameRef = useRef(null);
 
-  return useCallback((canvas, imageBitmap) => {
-    if (!hasTransferedControlRef.current) {
-      hasTransferedControlRef.current = true;
-      offscreenRef.current = canvas.transferControlToOffscreen();
-      workerRef.current.postMessage(
-        { event: "INIT", payload: { canvas: offscreenRef.current } },
-        [offscreenRef.current]
-      );
-    } else {
-      workerRef.current.postMessage({
-        event: "VIDEO_FRAME",
-        payload: { imageBitmap },
-      });
+  useEffect(() => {
+    if (canvas == null || stream == null) {
+      return;
+    }
+    canvasContextRef.current = canvas.getContext("bitmaprenderer");
+    videoTrackRef.current = stream.getVideoTracks()[0];
+    imageCaptureRef.current = new ImageCapture(videoTrackRef.current);
+  }, [canvas, stream]);
+
+  useEffect(() => {
+    if (requestAnimationFrameRef.current != null || canvas == null) {
+      return;
     }
 
-    // canvas.width = videoWidth;
-    // canvas.height = videoHeight;
-    // contextRef.current = canvas.getContext("2d");
-    // contextRef.current.drawImage(video, 0, 0, videoWidth, videoHeight);
-    // console.log("render shit");
-  }, []);
+    function render() {
+      imageCaptureRef.current
+        ?.grabFrame()
+        .then((imageBitmap) => {
+          canvas.width = imageBitmap.width;
+          canvas.height = imageBitmap.height;
+          canvasContextRef.current?.transferFromImageBitmap(imageBitmap);
+        })
+        .catch(() => {
+          /**
+           * see https://stackoverflow.com/questions/56747195/is-there-a-way-to-send-video-data-from-a-video-tag-mediastream-to-an-offscreenca
+           */
+        });
+
+      requestAnimationFrameRef.current = window.requestAnimationFrame(render);
+    }
+
+    requestAnimationFrameRef.current = window.requestAnimationFrame(render);
+
+    return () => window.cancelAnimationFrame(requestAnimationFrameRef);
+  }, [canvas]);
 }
